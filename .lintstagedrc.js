1b521658f2ad3956c68a35db2bc5dbe1d811c883
Fleen5l77 <fleen4321@outlook.com>
2026-03-02 16:28:28 +0300

    Delete .lintstagedrc.js

--- .lintstagedrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  '*.lib/**/*': ['prettier --write', 'eslint --fix'],
-  '*.{js,css,md}': 'prettier --write',
-  '*.ts': [() => 'tsc --skipLibCheck --noEmit'],
-  'package.json': ['prettier-package-json --write'],
-};
